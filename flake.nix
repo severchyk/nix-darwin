28421da6aab4dd0b0a58704d742c80f1038ded0a
Severyn Matsiak <severus008@gmail.com>
2025-12-05 17:33:47 +0200

    Add shell aliases

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
       environment.systemPackages = with pkgs; [
         awscli2
         bat
+        coreutils-prefixed
         fzf
         git
         google-chrome
@@ -90,9 +91,6 @@
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
--- home.nix
+++ home.nix
@@ -7,10 +7,110 @@
     stateVersion = "26.05";
   };
 
+  # Configure ~/.bashrc
+  programs.bash = {
+    enable = true;
+
+    bashrcExtra = ''
+      neofetch
+    '';
+
+    initExtra = ''
+      du() {
+        command gdu -h --max-depth 1 "$@" | sort -hr
+      }
+      rssh() {
+        ssh $(grep -E '^Host ' ~/.ssh/config | cut -d ' ' -f2 | sort | fzf)
+      }
+      rmc() {
+        MC_PROFILE_ROOT=~/.mc-server-connect mc sftp://$(grep -E '^Host ' ~/.ssh/config | cut -d ' ' -f2 | sort | fzf)
+      }
+    '';
+
+    shellAliases = {
+      # Add colors for filetype and human-readable sizes by default on 'ls':
+      ls = "gls -h --color=auto"; # Use GNU Coreutils ls version.
+      lx = "ls -lXB";  # Sort by extension.
+      lk = "ls -lSr";  # Sort by size, biggest last.
+      lt = "ls -ltr";  # Sort by date, most recent last.
+      lc = "ls -ltcr"; # Sort by/show change time, most recent last.
+      lu = "ls -ltur"; # Sort by/show access time, most recent last.
+
+      # The ubiquitous 'll': directories first, with alphanumeric sorting:
+      ll = "ls -alv --group-directories-first";
+      lm = "ll | more";    #  Pipe through 'more'
+      lr = "ll -R";        #  Recursive ls.
+      la = "ll -A";        #  Show hidden files.
+      tree = "tree -Csuh"; #  Nice alternative to 'recursive ls' ...
+
+      grep = "grep --color=auto";
+      fgrep = "fgrep --color=auto";
+      egrep = "egrep --color=auto";
+
+      ".." = "cd ..";
+    };
+
+    shellOptions = [
+      "checkwinsize"
+      "globstar"
+      "histappend"
+    ];
+  };
+
+  # Configure ~/.zshrc
+  programs.zsh = {
+    enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "strug";
+    };
+
+    loginExtra = ''
+      neofetch
+    '';
+
+    shellAliases = {
+      # Add colors for filetype and human-readable sizes by default on 'ls':
+      ls = "gls -h --color=auto"; # Use GNU Coreutils ls version.
+      lx = "ls -lXB";  # Sort by extension.
+      lk = "ls -lSr";  # Sort by size, biggest last.
+      lt = "ls -ltr";  # Sort by date, most recent last.
+      lc = "ls -ltcr"; # Sort by/show change time, most recent last.
+      lu = "ls -ltur"; # Sort by/show access time, most recent last.
+
+      # The ubiquitous 'll': directories first, with alphanumeric sorting:
+      ll = "ls -alv --group-directories-first";
+      lm = "ll | more";    #  Pipe through 'more'
+      lr = "ll -R";        #  Recursive ls.
+      la = "ll -A";        #  Show hidden files.
+      tree = "tree -Csuh"; #  Nice alternative to 'recursive ls' ...
+
+      grep = "grep --color=auto";
+      fgrep = "fgrep --color=auto";
+      egrep = "egrep --color=auto";
+
+      ".." = "cd ..";
+    };
+
+    siteFunctions = {
+      du = ''
+        command gdu -h --max-depth 1 "$@" | sort -hr
+      '';
+      rssh = ''
+        ssh $(grep -E '^Host ' ~/.ssh/config | cut -d ' ' -f2 | sort | fzf)
+      '';
+      rmc = ''
+        MC_PROFILE_ROOT=~/.mc-server-connect mc sftp://$(grep -E '^Host ' ~/.ssh/config | cut -d ' ' -f2 | sort | fzf)
+      '';
+    };
+  };
+
   # Install Ghostty.
   programs.ghostty = {
     enable = true;
     enableBashIntegration = true;
+    enableZshIntegration = true;
 
     # Specify darwin package.
     package = pkgs.ghostty-bin;
