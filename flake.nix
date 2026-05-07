ab23e01ce47eda4c147f82916e725453d8106f30
Severyn Matsiak <severus008@gmail.com>
2026-05-08 00:12:23 +0300

    Remove default Terminal

--- flake.nix
+++ flake.nix
@@ -50,7 +50,6 @@
             "${pkgs.google-chrome}/Applications/Google Chrome.app"
             "${pkgs.slack}/Applications/Slack.app"
             "${pkgs.ghostty-bin}/Applications/Ghostty.app"
-            "/System/Applications/Utilities/Terminal.app"
             # "${pkgs.warp-terminal}/Applications/Warp.app"
             "${pkgs.antigravity}/Applications/Antigravity.app"
             "${pkgs.vscode}/Applications/Visual Studio Code.app"
