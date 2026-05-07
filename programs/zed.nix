96a10a3ee7b9dbe4595f69561d78f7f7ae8d4761
Severyn Matsiak <severus008@gmail.com>
2026-05-07 17:38:27 +0300

    Change extensions

--- programs/zed.nix
+++ programs/zed.nix
@@ -7,19 +7,19 @@
     # This populates the userSettings "auto_install_extensions"
     extensions = [
       "dockerfile"
+      "groovy"
       "ini"
+      "nginx"
       "nix"
       "rainbow-csv"
       "terraform"
-      "blade-runner-2049"
+
       "catppuccin"
       "catppuccin-blur"
       "catppuccin-icons"
       "github-theme"
       "macos-classic"
       "tokyo-night"
-      "vscode-dark-modern"
-      "vscode-icons"
     ];
 
     # Everything inside of these brackets are Zed options
